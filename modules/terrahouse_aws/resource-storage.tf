b3993ac6b6d256059716ea1530b66e03a6abd80d
Tony Osunde <mrtonero4@gmail.com>
2023-10-06 20:47:04 +0000

    ficing issues with variables

--- modules/terrahouse_aws/resource-storage.tf
+++ modules/terrahouse_aws/resource-storage.tf
@@ -14,15 +14,17 @@ resource "aws_s3_object" "index-object" {
   bucket = aws_s3_bucket.website_bucket.id
   key    = "index.html"
   content_type = "text/html"
-  source = var.index_html_filepath
-  etag = filemd5(var.index_html_filepath)
+  source = "${path.root}${var.index_html_filepath}"
+  etag = filemd5("${path.root}${var.index_html_filepath}")
 }
+
+
 resource "aws_s3_object" "error-object" {
   bucket = aws_s3_bucket.website_bucket.id
   key = "error.html"
   content_type = "text/html"
-  source = var.error_html_filepath
-  etag = filemd5(var.error_html_filepath)
+  source = "${path.root}${var.error_html_filepath}"
+  etag = filemd5("${path.root}${var.index_html_filepath}")
 }
 #https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_website_configuration
 resource "aws_s3_bucket_website_configuration" "website_configuration" {
