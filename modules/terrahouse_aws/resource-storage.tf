227b23cff082d0c1c7bf1779c1e5e52ca70c60a4
Tony Osunde <mrtonero4@gmail.com>
2023-10-06 22:12:21 +0000

    new destroy

--- modules/terrahouse_aws/resource-storage.tf
+++ modules/terrahouse_aws/resource-storage.tf
@@ -10,22 +10,22 @@ resource "aws_s3_bucket" "website_bucket" {
 }
 
 #https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_object
-resource "aws_s3_object" "index-object" {
-  bucket = aws_s3_bucket.website_bucket.id
-  key    = "index.html"
-  content_type = "text/html"
-  source = "${path.root}${var.index_html_filepath}"
-  etag = filemd5("${var.index_html_filepath}")
-}
+# resource "aws_s3_object" "index-object" {
+#   bucket = aws_s3_bucket.website_bucket.id
+#   key    = "index.html"
+#   content_type = "text/html"
+#   source = "${path.root}${var.index_html_filepath}"
+#   etag = filemd5("${var.index_html_filepath}")
+# }
 
 
-resource "aws_s3_object" "error-object" {
-  bucket = aws_s3_bucket.website_bucket.id
-  key = "error.html"
-  content_type = "text/html"
-  source = "${path.root}${var.error_html_filepath}"
-  etag = filemd5("${var.error_html_filepath}")
-}
+# resource "aws_s3_object" "error-object" {
+#   bucket = aws_s3_bucket.website_bucket.id
+#   key = "error.html"
+#   content_type = "text/html"
+#   source = "${path.root}${var.error_html_filepath}"
+#   etag = filemd5("${var.error_html_filepath}")
+# }
 #https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_website_configuration
 resource "aws_s3_bucket_website_configuration" "website_configuration" {
   bucket = aws_s3_bucket.website_bucket.id
