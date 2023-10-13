a9ea7f2b8e35e08f96307912ba0a8d3e637061c5
cloudtonero <anthonyosunde45@gmail.com>
2023-10-13 23:08:21 +0000

    changed terrahouse to ganers-grotto and cooker-cove for final submission

--- main.tf
+++ main.tf
@@ -36,7 +36,7 @@ resource "terratowns_home" "arcanum" {
   description     = <<DESCRIPTION
 God of War Ragnarök is an action-adventure game developed by Santa Monica Studio and published by Sony Interactive Entertainment. It was released worldwide on November 9, 2022, for both the PlayStation 4 and PlayStation 5, marking the first cross-gen release in the God of War series. It is the ninth installment in the series, the ninth chronologically, and the sequel to 2018's God of War. Loosely based on Norse mythology, the game is set in ancient Scandinavia and features series protagonist, Kratos, and his now teenage son, Atreus. Concluding the Norse era of the series, the game covers Ragnarök, the eschatological event which is central to Norse mythology and was foretold to happen in the previous game after Kratos killed the Aesir god Baldur.
 DESCRIPTION
-  town            = "cooker-cove"
+  town            = "gamers-grotto"
   content_version = var.arcanum.content_version
   domain_name     = module.home_arcanum.cdn_domain_name
 }
